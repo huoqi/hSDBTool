353058eaeebd871caca72a6aca4788ef7e3db935
huoqi <https://github.com/huoqi>
2014-09-16 22:43:33 +0800

    Update jdk

--- src/com/aws/dialog/DeleteDomainConfirmDialog.java
+++ src/com/aws/dialog/DeleteDomainConfirmDialog.java
@@ -7,13 +7,13 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
 
-public class ReleteDomainConfirmDialog extends Dialog {
+public class DeleteDomainConfirmDialog extends Dialog {
 
 	/**
 	 * Create the dialog.
 	 * @param parentShell
 	 */
-	public ReleteDomainConfirmDialog(Shell parentShell) {
+	public DeleteDomainConfirmDialog(Shell parentShell) {
 		super(parentShell);
 	}
 
